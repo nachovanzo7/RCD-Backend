6532dd9a5171a233bd1b23bea04c0c66dd83984c
Mauriciourrozz <mauriciourrozz@gmail.com>
2025-03-10 10:49:54 -0300

    Create databases

--- rcdproject/rcdproject/settings.py
+++ rcdproject/rcdproject/settings.py
@@ -114,10 +114,15 @@ WSGI_APPLICATION = 'rcdproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestion_rcd_database',
+        'USER': 'rcdgestion',
+        'PASSWORD': 'rcd_gestion',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
